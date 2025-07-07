a0826c54cd200c4ec59fc2fb0a8ed1ab5e37f665
Florent Uzio <fuzio@ripple.com>
2025-07-07 17:46:37 +0200

    dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,29 @@
-import { BasePath, Fireblocks } from "@fireblocks/ts-sdk";
-import { readFileSync } from 'fs';
+import { BasePath, Fireblocks } from "@fireblocks/ts-sdk"
+import { readFileSync } from "fs"
+import { config } from "dotenv"
 
-const FIREBLOCKS_API_SECRET_PATH = "./fireblocks_secret.key";
+config()
+
+const { FIREBLOCKS_API_KEY = "" } = process.env
+
+const FIREBLOCKS_API_SECRET_PATH = "./fireblocks_secret.key"
 
 // Initialize a Fireblocks API instance with local variables
 const fireblocks = new Fireblocks({
-  apiKey: "",
-  basePath: BasePath.Sandbox, // or assign directly to "https://sandbox-api.fireblocks.io/v1";
+  apiKey: FIREBLOCKS_API_KEY,
+  basePath: BasePath.US, // or assign directly to "https://sandbox-api.fireblocks.io/v1";
   secretKey: readFileSync(FIREBLOCKS_API_SECRET_PATH, "utf8"),
-});
+})
 
 //retrive vault accounts
 async function getVaultPagedAccounts(limit: number) {
   try {
     const vaults = await fireblocks.vaults.getPagedVaultAccounts({
       limit,
-    });
-    return vaults;
+    })
+    return vaults
   } catch (e) {
-    console.log(e);
+    console.log(e)
   }
 }
 
@@ -38,16 +43,16 @@ const transaction = async () => {
       amount: "0.1",
       note: "Test transaction",
       extraParameters: {
-        // ...
-      }
-    }
-  });
+        type: {},
+      },
+    },
+  })
 }
 
 getVaultPagedAccounts(10)
   .then((res) => {
-    console.log(res);
+    console.log(res?.data)
   })
   .catch((e) => {
-    console.log(e);
-  });
+    console.log(e)
+  })
